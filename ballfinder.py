197473e9c6fda08e958eae6c344a9ee4f157c2f3
LarsSjosund <lassolass@gmail.com>
2015-03-29 17:13:49 +0200

    simple logic for finding non-occluded ball

--- ballfinder.py
+++ ballfinder.py
@@ -11,7 +11,8 @@ def render_blob(layer, blob):
         color=cv.Color.RED
     )
 
-img = cv.Image('/home/sjosund/Programming/Pictures/IMG_4931.JPG').resize(1200)
+filename = 'test.JPG'
+img = cv.Image(filename).resize(1200)
 ball_img = img.colorDistance((255,177,38)).binarize(40)
 blobs = filter(lambda b:b.isCircle(0.3), ball_img.findBlobs())
 
